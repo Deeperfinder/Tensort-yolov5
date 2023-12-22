b6e311fa85d83b204f453d2e8ef702df05461522
alex <867377995@qq.com>
2023-12-22 12:07:14 +0800

    delete build file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /work/simple_yolov5_demo
-# Build directory: /work/simple_yolov5_demo/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
